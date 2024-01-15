b7d424624afb3c3356d031d1ca2c07e7c8214473
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-01-15 23:04:27 +0600

    another solution of 929th solution

--- 929_Unique_Email_Addresses.py
+++ 929_Unique_Email_Addresses.py
@@ -1,3 +1,4 @@
+# using built in module
 class Solution(object):
     def numUniqueEmails(self, emails):
         unique = set()
@@ -8,3 +9,18 @@ class Solution(object):
             unique.add((local, domain))
         return len(unique)
     
+# without using built in module
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        unique = set()
+        for e in emails:
+            i, local = 0, ''
+            while e[i] not in ['@', '+']:
+                if e[i] != '.':
+                    local += e[i]
+                i += 1
+            while e[i] != '@':
+                i += 1
+            domain = e[i + 1:]
+            unique.add((local, domain))       
+        return len(unique)
