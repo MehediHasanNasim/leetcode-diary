02e8e63e8dc3d5e11dfb990f22424ad14448045a
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-01-22 04:13:43 +0600

    adding another 344th problem

--- 344_Reverse_String.py
+++ 344_Reverse_String.py
@@ -4,7 +4,17 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
+        # two pointer // time o(n), space o(1)
         l ,r = 0, len(s) - 1
         while l < r:
             s[l], s[r] = s[r], s[l]
             l , r = l + 1, r - 1
+            
+        # Stack // time o(n), space o(n)
+        stack = []
+        for c in s:
+            stack.append(c)
+        i = 0 
+        while stack:
+            s[i] = stack.pop()
+            i += 1
