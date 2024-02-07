6c5c218f72d65094ff4f1aa12a39ab5c844e79a8
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-02-07 16:25:06 +0600

    adding 225th solution

---
+++ 225_Implement_Stack_using_Queues.py
@@ -0,0 +1,24 @@
+class MyStack(object):
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x):
+        self.stack.append(x)
+
+    def pop(self):
+        if not self.empty():
+            return self.stack.pop()
+        else:
+            return False
+
+    def top(self):
+        if not self.empty():
+            return self.stack[-1]
+        else:
+            return False
+    
+
+    def empty(self):
+        return len(self.stack) == 0
+        
