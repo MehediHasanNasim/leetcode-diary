08ad91fb91eeca70265fbabf4fcfb7d3fdc89eaf
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-02-13 04:29:32 +0600

    editing 977th solution

---
+++ 977_Squares_of_a_Sorted_Array.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+    
+        #using loop
+        res = []
+        for i in nums:
+            res.append(i*i)
+        return sorted(res)
+            
+        # short way of looping
+        squares = [num*num for num in nums] 
+        return sorted(squares)
+
+        # two pointer solution
+        res = []
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            if nums[l] * nums[l] > nums[r] * nums[r]:
+                res.append(nums[l] * nums[l])
+                l += 1
+            else:
+                res.append(nums[r] * nums[r])
+                r -= 1
+        return res[::-1]
