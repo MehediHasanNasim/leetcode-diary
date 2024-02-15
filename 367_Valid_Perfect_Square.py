792a407e2172d5d0eae73a95b13f420dacc0259b
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-02-15 18:04:13 +0600

    editing 367th solution

--- 367_Valid_Perfect_Square.py
+++ 367_Valid_Perfect_Square.py
@@ -12,3 +12,11 @@ class Solution(object):
             else:
                 r = mid - 1
         return False
+
+
+        # brute force >> though- here memory extend crossing
+        for i in range(1, num + 1):
+            if i*i == num:
+                return True
+            elif i*i > num:
+                return False
