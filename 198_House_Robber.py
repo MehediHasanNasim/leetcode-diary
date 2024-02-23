b3378fd10158d353eac0be4d02c9b07934030c26
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-02-23 18:09:29 +0600

    editing 198th solution

--- 198_House_Robber.py
+++ 198_House_Robber.py
@@ -3,7 +3,8 @@ class Solution(object):
        
         rob1, rob2 = 0, 0 
         for n in nums:
-            temp = max(n + rob1, rob2)
+            Temp = max(n + rob1, rob2)
             rob1 = rob2
-            rob2 = temp
+            rob2 = Temp
         return rob2
+    
