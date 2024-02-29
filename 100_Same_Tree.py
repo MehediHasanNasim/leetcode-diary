e8816f1ede10d2c097b6ae95a9474206f26b788f
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-02-29 18:39:02 +0600

    edting 100th solution

--- 100_Same_Tree.py
+++ 100_Same_Tree.py
@@ -4,11 +4,13 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution(object):
     def isSameTree(self, p, q):
         
         if not p and not q:
             return True
+        
         if not p or not q or p.val != q.val:
             return False 
         
