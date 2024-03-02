cbd2cbc3469baaeece5aab671392064e98edc534
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-03-02 20:51:08 +0600

    adding 168th solution

---
+++ 168_Excel_Sheet_Column_Title.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def convertToTitle(self, columnNumber):
+        
+        res = ""
+        while columnNumber > 0:
+            offset = (columnNumber - 1) % 26
+            res += chr(ord('A') + offset) 
+            columnNumber = (columnNumber - 1) // 26
+        return res[::-1]
