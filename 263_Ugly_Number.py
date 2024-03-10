4bac1d66e81c8127154e498b1dcfb8730a26ab45
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-03-10 11:08:21 +0600

    editing 263th solution

--- 263_Ugly_Number.py
+++ 263_Ugly_Number.py
@@ -3,7 +3,10 @@ class Solution(object):
         
         if n <= 0:
             return False
+        
         for p in [2,3,5]:
             while n % p == 0:
                 n = n // p 
+                
         return n == 1
+    
