e9abca398eb2b08de5e40bddfec8fd68fc9dc9ec
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-03-12 15:16:50 +0600

    editing 410th solution

--- 410_Split_Array_Largest_Sum.py
+++ 410_Split_Array_Largest_Sum.py
@@ -1,5 +1,6 @@
 class Solution(object):
-    def splitArray(self, nums, k):
+    def splitArray(self, nums, k):  
+
         def canSplit(largest):
             subarray = 0
             curSum = 0
@@ -7,38 +8,15 @@ class Solution(object):
                 curSum += n
                 if curSum > largest:
                     subarray += 1
-                    curSum = n
+                    curSum = n         
             return subarray + 1 <= k
         l, r = max(nums), sum(nums)
         res = r
         while l <= r:
-            mid = l + ((r - 1) // 2)
+            mid = l + ((r - l) // 2)
             if canSplit(mid):
                 res = mid
                 r = mid - 1
             else:
                 l = mid + 1
         return res
-
-
-
-        # def canSplit(largest):
-        #     subarray = 0
-        #     curSum = 0
-        #     for n in nums:
-        #         curSum += n
-        #         if curSum > largest:
-        #             subarray += 1
-        #             curSum = n
-        #     return subarray + 1 <= k
-
-        # l, r = max(nums), sum(nums)
-        # res = r
-        # while l <= r:
-        #     mid = l + ((r - l) // 2)
-        #     if canSplit(mid):
-        #         res = mid
-        #         r = mid - 1
-        #     else:
-        #         l = mid + 1
-        # return res
