650a3f11836c2705b9aa4b94a2b270112d90769b
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-03-15 12:19:52 +0600

    adding 1572th solution

---
+++ 1572_Matrix_Diagonal_Sum.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def diagonalSum(self, mat):
+        
+        res, n = 0, len(mat)
+
+        for i in range(n):
+            res += mat[i][i] #primary diagonal
+            res += mat[i][n-1-i] #secondary diagonal
+
+        return res - (mat[n // 2][n // 2] if n % 2 else 0)
