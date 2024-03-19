575cfbad783097c8f20fe75b3aafd6b53096d151
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-03-19 12:39:23 +0600

    editing 1470th solution

--- 1470_Shuffle_the_Array.py
+++ 1470_Shuffle_the_Array.py
@@ -7,3 +7,18 @@ class Solution(object):
             res.append(nums[i])
             res.append(nums[i+n])
         return res
+
+
+        # Bit manipulation
+        for i in range(n):
+            nums[i] = nums[i] << 10
+            nums[i] = nums[i] | nums[i + n]
+        
+        j = 2 * n - 1
+        for i in range(n - 1, -1, -1):
+            y = nums[i] & (2**10 - 1)
+            x = nums[i] >> 10
+            nums[j] = y
+            nums[j - 1] = x
+            j -= 2
+        return nums
