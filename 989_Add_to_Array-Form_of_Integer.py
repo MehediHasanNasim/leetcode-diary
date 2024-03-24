7a5ab115e1e6fd44a4be59bf46fec98abd7cd8ba
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-03-24 14:03:11 +0600

    adding another solution of 989th problem solution

--- 989_Add_to_Array-Form_of_Integer.py
+++ 989_Add_to_Array-Form_of_Integer.py
@@ -13,3 +13,24 @@ class Solution(object):
         return output_res
 
 
+
+        # Bit manipulation
+        num.reverse()
+        i = 0
+        while k:
+            digit = k % 10
+            if i < len(num):
+                num[i] += digit
+            else:
+                num.append(digit)
+            
+            carry = num[i] // 10
+            num[i] = num[i] % 10
+
+            k = k // 10
+            k += carry
+            i += 1
+        num.reverse()
+
+        return num
+
