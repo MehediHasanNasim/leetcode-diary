efde1e532f303e153bb1b3f3aa78913adddd4b25
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-04-13 20:03:52 +0600

    editing solution of 1603th problem

--- 1603_Design_Parking_System.py
+++ 1603_Design_Parking_System.py
@@ -1,18 +1,11 @@
 class ParkingSystem(object):
 
     def __init__(self, big, medium, small):
-        """
-        :type big: int
-        :type medium: int
-        :type small: int
-        """
+       
         self.spaces = [big, medium, small]
 
     def addCar(self, carType):
-        """
-        :type carType: int
-        :rtype: bool
-        """
+     
         if self.spaces[carType - 1] > 0:
             self.spaces[carType - 1] -= 1
             return True
