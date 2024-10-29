5c0976cb2a98f9942016587048e7105d4473a207
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2024-10-30 01:40:32 +0600

    adding 1963th solution

---
+++ 1963_Minimum_Number_of_Swaps_to_Make_the_String_Balanced.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minSwaps(self, s):
+        close, maxClose = 0, 0
+
+        for c in s:
+            if c == "[":
+                close -= 1
+            else:
+                close += 1
+
+                maxClose = max(close, maxClose)
+        return (maxClose + 1) // 2
+        
