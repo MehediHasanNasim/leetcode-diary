bc59e5f45563d94c5a45abb5abff112d41cac4b0
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2025-03-07 02:15:17 +0600

    adding 2965th problem solution

---
+++ 2965_Find_Missing_and_Repeated_Values.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findMissingAndRepeatedValues(self, grid):
+        N = len(grid)
+        count = defaultdict(int)
+
+        for i in range(N):
+            for j in range(N):
+                count[grid[i][j]] += 1
+            
+        double, missing = 0, 0
+        for num in range(1, N*N + 1):
+            if count[num] == 0:
+                missing = num
+            elif count[num] == 2:
+                double = num
+
+        return [double, missing]
